eb9027619266b4b5a7a27a730ef4e80304988299
Miljana0609 <miljanazoranovic25@gmail.com>
2026-03-24 14:49:57 +0100

    Change text

--- src/components/InternshipSection.tsx
+++ src/components/InternshipSection.tsx
@@ -37,7 +37,7 @@ const InternshipSection: FC = () => {
       <div className="flex items-center gap-3 mb-4">
             <div className="h-px flex-1 bg-border" />
             <span className="text-xs tracking-widest uppercase text-muted-foreground">
-              LIA
+            Internship
             </span>
             <div className="h-px flex-1 bg-border" />
           </div>
